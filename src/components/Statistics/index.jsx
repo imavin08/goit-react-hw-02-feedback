05a6ac4b272485fabde8806bd8cde6f6db302470
Imavin08 <92112933+Imavin08@users.noreply.github.com>
2022-07-07 21:52:14 +0200

    add Tast 1 FeedBack

---
+++ src/components/Statistics/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import css from './Statistics.module.css';
+
+const Statistics = ({ good, neutral, bad, total, positivePercentage }) => (
+  <div>
+    <p className={css.item}>Good: {good}</p>
+    <p className={css.item}>Neutral: {neutral}</p>
+    <p className={css.item}>Bad: {bad}</p>
+
+    <p className={css.item}>Total: {total}</p>
+    <p className={css.item}>Positive feedback: {positivePercentage}%</p>
+  </div>
+);
+
+export default Statistics;
+
+Statistics.propTypes = {
+  good: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.number,
+  positivePercentage: PropTypes.number,
+};
