90298edb6a53fd50226f830218172bcb97c8aff3
Imavin08 <92112933+Imavin08@users.noreply.github.com>
2022-07-10 22:31:39 +0200

    FixBug
    Fix props in components
    Nitification
    Statistics

--- src/components/Statistics/index.jsx
+++ src/components/Statistics/index.jsx
@@ -17,7 +17,8 @@ export default Statistics;
 
 Statistics.propTypes = {
   good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
   bad: PropTypes.number.isRequired,
-  total: PropTypes.number,
-  positivePercentage: PropTypes.number,
+  total: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.number.isRequired,
 };
